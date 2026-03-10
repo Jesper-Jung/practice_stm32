d6a857fa0c6cf10b0e884f2f1db39c0ffadc2773
Jesper Jung <3388jung@snu.ac.kr>
2026-03-10 16:16:01 +0900

    Add some needed HAL driver files as source files
    > These driver files related for bsp
        - HAL driver initialization
        - System clock configuration
        - ...

--- Src/bsp/bsp.h
+++ Src/bsp/bsp.h
@@ -2,10 +2,14 @@
 #define SRC_BSP_BSP_H_
 
 #include "def.h"
+#include "stm32h5xx_hal.h"
+#include "stm32h5xx_nucleo.h"
 
 void bspInit(void);
 
 void delay(uint32_t ms);
 uint32_t millis(void);
+void SystemClock_Config(void);
+void Error_Handler(void);
 
 #endif
